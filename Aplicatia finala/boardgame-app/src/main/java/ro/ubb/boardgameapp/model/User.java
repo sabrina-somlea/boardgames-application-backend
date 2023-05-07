7d5829f604712f6254eeb13a465039376a116604
Sabrina-Daniela Groza <sabrina.somlea@gmail.com>
2023-05-07 16:13:14 +0300

    fix registration form

--- Aplicatia finala/boardgame-app/src/main/java/ro/ubb/boardgameapp/model/User.java
+++ Aplicatia finala/boardgame-app/src/main/java/ro/ubb/boardgameapp/model/User.java
@@ -1,5 +1,6 @@
 package ro.ubb.boardgameapp.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import lombok.*;
 
@@ -16,6 +17,7 @@ import java.util.UUID;
 public class User extends BaseEntity<UUID> {
     private String firstName;
     private String lastName;
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date birthday;
     private Gender gender;
     private String email;
